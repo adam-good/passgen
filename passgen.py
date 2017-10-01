dc7fe637a41845b423521cae93ec02bc6006a453
Adam Good <dev.adamgood@gmail.com>
2017-09-30 18:49:18 -0700

    Cleaned code and remove 'Salt' feature

--- passgen.py
+++ passgen.py
@@ -1,90 +1,65 @@
 '''
 *   File:       passgen.py
 *   Author:     Adam Good
-*   Version:    1.0
+*   Version:    1.1
 '''
 import sys
 import random
-import string
-
-wordList = []
-listLen = 0
 
 def main():
-    global wordList
-    global listLen
-
-    if len(sys.argv) < 5:
+    """Script Starts Here"""
+    argc = len(sys.argv)
+    if argc < 5:
         print "Usage: passgen.py <min_password_length> <max_password_length> <number_of_passwords> <wordlist>"
         return
-    # Initialize some important variables
-    minPassLen = int(sys.argv[1])
-    maxPassLen = int(sys.argv[2])
-    passNum = int(sys.argv[3])      # Number of passwords
-    wordfile = open(sys.argv[4])
 
-    # Read the wordlist file into a list of words without the \n
-    wordList = [w.replace('\n', '') for _,w in enumerate(wordfile)]
-    listLen = len(wordList)
+    min_pass_len = int(sys.argv[1])
+    max_pass_len = int(sys.argv[2])
+    number_of_passwords = int(sys.argv[3])
+    word_list_file = open(sys.argv[4])
 
-    # This loop generates each password
-    for i in range(0, passNum):
-        password = genPassword(minPassLen, maxPassLen)
-        print(str(i+1) + ".) " + str(password))
+    word_list = [w.replace("\n", "").replace("'", "") for _, w in enumerate(word_list_file)]
+    for i in range(number_of_passwords):
+        password = gen_password(min_pass_len, max_pass_len, word_list)
+        print str(i) + ".) " + password
 
-# This function actually generates the passwords
-def genPassword(minPassLen, maxPassLen):
-    # These are the global variables of our list and its length
-    global wordList
-    global listLen
-
-    # Prep our variables
+def gen_password(min_pass_len, max_pass_len, word_list):
+    """Generate A Password Based On Arguments"""
     password = ""
-    minPassLen = minPassLen - 5 # For 5 character salt
-    maxPassLen = maxPassLen - 5
 
-    # While the password is too short keep adding words to the password
-    while len(password) < minPassLen:
-        # Generate a random number to grab a random word and append it to password
-        r = random.randint(0, listLen)
-        password += wordList[r].replace("'", '')
-        password += "_" # These are to help memorization by making it more readable
-        # If the password is too long try again.
-        # TODO detect this earlier so we don't have to gen an entire new password
-        if len(password) > maxPassLen:
+    list_len = len(word_list)
+    while len(password) < min_pass_len:
+        rand = random.randint(0, list_len-1)
+        word = word_list[rand]
+        password += word
+
+        #TODO Find more effecient way of handling this
+        if len(password) > max_pass_len:
             password = ""
-    # Append additional 5 char salt to the end for 26^5 extra combinations
-    password += genSalt()
-    # Add 2 new rules to our password; e.g. 'a' = '4' or 'y' = 'Y'
-    password = randomRule(password)
+
+    password = random_replace_cipher(password)
+
     return password
 
-# This function just generates a 5 character salt from uppercase alphabet
-def genSalt():
-    salt = ""
-    for i in range(0, 5):
-        r = random.randint(0, 25)
-        salt += string.ascii_uppercase[r]
-    return salt
+def random_replace_cipher(password):
+    """Runs a replacement cipher on the password. Replacements are chosen at random"""
 
-# This function just changes 2 characteristics of the password.
-# TODO make this slightly more complicated
-def randomRule(password):
-    for x in range(0, 2):
-        # Get a random letter and implement its specific rule
-        i = random.randint(0, 25)
-        if string.ascii_lowercase[i] == 'a':
-            password = password.replace('a', '4')
-        elif string.ascii_lowercase[i] == 'e':
-            password = password.replace('e', '3')
-        elif string.ascii_lowercase[i] == 'l':
-            password = password.replace('l', '1')
-        elif string.ascii_lowercase[i] == 't':
-            password = password.replace('t', '7')
-        elif string.ascii_lowercase[i] == 's':
-            password = password.replace('s', '5')
-        else:
-            password = password.replace(string.ascii_lowercase[i], string.ascii_uppercase[i])
+    # Literally just using "1337 Speak" for the replacements
+    letters = ['a', 'e', 'l', 't', 's']
+    numbers = ['4', '3', '1', '7', '5']
+
+    # Currently it will replace between 1 and 3 of the letters.
+    #TODO Maybe give user options to decide this
+    for _ in range(1, 3):
+        i = random.randint(0, len(letters)-1)
+        letter = letters[i]
+        number = numbers[i]
+
+        password = password.replace(letter, number)
+
+        letters.remove(letter)
+        numbers.remove(number)
+
     return password
 
 main()
